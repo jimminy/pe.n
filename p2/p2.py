7d3db1aa303dd086a9e3044752206ab3ceee8983
James Fuller <jimminyfuller@gmail.com>
2011-11-21 06:45:13 -0500

    Iterative Fibonacci, using list for storage, with cutoff at 4MM

--- p2/p2.py
+++ p2/p2.py
@@ -1,14 +1,15 @@
 #For all Fibonacci numbers less than n find the sum of all even-valued terms
 
-def fib(n):
-    if(n==0 or n==1):
+def fib(fib_seq):
+    num = len(fib_seq)
+    if(num==0 or num==1):
         return 1
     else:
-        return fib(n-1)+fib(n-2)
+        return fib_seq[-1]+fib_seq[-2]
 
-i = 0
+seq = []
 x = 0
 while(x<4000000):
-    x = fib(i)
-    i = i+1
+    seq.append(fib(seq))
+    x = seq[-1]
     print x
